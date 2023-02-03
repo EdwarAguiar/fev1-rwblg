de306065667e0653a41754b00c397bc1af80ed29
Aguiar Edwar <Aguiar.Edwar@gmail.com>
2023-02-03 08:20:25 -0600

    [17] - Component NavBar adjusted to fit the screen

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContex'
 import { SubmitButton } from '../components/SubmitButton'
+import { Frame } from '../styles/styles_user'
 
 export const User = () => {
   const { removeAuth } = useContext(AppContext)
@@ -8,7 +9,14 @@ export const User = () => {
   return (
     <>
       <h1>User Profile</h1>
-      <SubmitButton onClick={removeAuth}> Cerrar Sesion</SubmitButton>
+      <Frame>
+        <p>Brilla, brilla intensamente </p>
+        <p>Hasta que el Mundo, </p>
+        <p>se maraville con lo que ve </p>
+      </Frame>
+      <div>
+        <SubmitButton onClick={removeAuth}> Cerrar Sesion</SubmitButton>
+      </div>
     </>
   )
 }
---
+++ src/styles/styles_user.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Frame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:center;
+  margin-top: 100px;
+  margin-bottom: 200px;
+`
